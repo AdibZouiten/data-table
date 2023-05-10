43bd9143ac394ae048168dd3382281950a53aaf6
Adib Zouiten <adibzouiten2@gmail.com>
2023-05-10 22:58:37 +0100

    init

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,129 @@
+import React, { useEffect , useState } from 'react';
+import axios from 'axios';
+
+
+
+
+function Table() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.get(`https://sheet.best/api/sheets/19ee97b6-68ab-485c-bd50-db7b1a82a808`);
+            setData(response.data);
+            console.log(response.data);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+    
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <div class="container mx-auto px-4 sm:px-8">
+            <div class="py-8">
+                <div>
+                <h2 class="text-2xl font-semibold leading-tight">Influencers Database</h2>
+                </div>
+                <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div
+                    class="inline-block min-w-full shadow-md rounded-lg overflow-hidden"
+                >
+                    <table class="min-w-full leading-normal">
+                    <thead>
+                        <tr>
+                        <th
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                        >
+                            Influencer
+                        </th>
+                        <th
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                        >
+                            Followers
+                        </th>
+                        <th
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                        >
+                            Platform
+                        </th>
+                        <th
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                        >
+                            Category
+                        </th>
+                        <th
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                        >
+                            Email
+                        </th>
+                        <th
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                        >
+                            Bio
+                        </th>
+                        <th
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                        >
+                            Url
+                        </th>
+                        <th
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"
+                        ></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item) => (
+                            <tr key={item.email}>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <div class="flex">
+                                    <div class="ml-3">
+                                        <p class="text-gray-900 whitespace-no-wrap">
+                                        {item.Name}
+                                        </p>
+                                        <p class="text-gray-600 whitespace-no-wrap">{item.Handle}</p>
+                                    </div>
+                                    </div>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{item.Followers}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{item.Platform}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{item.Category}</p>
+                                </td>
+                                <td class="px-1 py-5  border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{item.Email}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{item.Platform}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <a href={item.Url}>
+                                    <button
+                                    type="button"
+                                    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                        <i class="fa-solid fa-link px-1 "></i>
+                                        View
+                                    </button>
+                                    </a>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    </table>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Table
